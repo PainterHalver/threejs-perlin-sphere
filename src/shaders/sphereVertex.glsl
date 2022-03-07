b98ecb7bbdc6611cba7995b696efcb57a73de830
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-03-07 21:19:45 +0700

    REAL shader init template

--- src/shaders/sphereVertex.glsl
+++ src/shaders/sphereVertex.glsl
@@ -1,11 +1,10 @@
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    vec3 pos = modelPosition.xyz;
 
-    // Do something with pos
+    // Do something with modelPosition.xyz
 
 
-    vec4 viewPosition = viewMatrix * pos;
+    vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
